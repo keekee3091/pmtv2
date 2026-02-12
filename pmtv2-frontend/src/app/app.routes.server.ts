072d16dfcba0ef2bd6929f77dc4c2baf0ff3845a
Keenan Guiet <keenanguiet@pci33.fr>
2026-02-12 23:16:42 +0100

    MAJ 12022026v2

---
+++ pmtv2-frontend/src/app/app.routes.server.ts
@@ -0,0 +1,7 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+
+export const serverRoutes: ServerRoute[] = [
+  { path: 'projects/:id/**', renderMode: RenderMode.Server },
+  { path: 'tasks/:id/**', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Prerender }
+];
